9c7944f16647a98bd495220a779d40f46e366701
nikita-t5 <nickita.tashirev@yandex.ru>
2021-04-13 20:25:07 +0300

    injecting values from AppCon/external file

--- src/main/java/ru/simpleSpringApp/MusicPlayer.java
+++ src/main/java/ru/simpleSpringApp/MusicPlayer.java
@@ -1,10 +1,37 @@
 package ru.simpleSpringApp;
 
 public class MusicPlayer {
-    Music music;
+    private Music music;
+    private String name;
+    private int volume;
+    private float price;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
 
     public void setMusic(Music music) { //DI через setter
         this.music = music;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public double getPrice(){
+        return price;
     }
 
     public void playMusic() {
--- src/main/java/ru/simpleSpringApp/TestSpring.java
+++ src/main/java/ru/simpleSpringApp/TestSpring.java
@@ -8,6 +8,9 @@ public class TestSpring {
                 "applicationContext.xml");
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class); //DI
         musicPlayer.playMusic();
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());
+        System.out.println(musicPlayer.getPrice());
         context.close();
     }
 }
