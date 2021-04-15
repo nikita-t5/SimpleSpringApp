30188c65bf8df89283aed102c4818bafcefa3037
nikita-t5 <nickita.tashirev@yandex.ru>
2021-04-15 19:55:33 +0300

    scopes: Singleton and Prototype

--- src/main/java/ru/simpleSpringApp/TestSpring.java
+++ src/main/java/ru/simpleSpringApp/TestSpring.java
@@ -6,11 +6,24 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml");
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class); //DI
-        musicPlayer.playMusicList();
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
-        System.out.println(musicPlayer.getPrice());
+        MusicPlayer firstMusicPlayerSingleton = context.getBean("musicPlayerSingleton", MusicPlayer.class);
+        MusicPlayer secondMusicPlayerSingleton = context.getBean("musicPlayerSingleton", MusicPlayer.class);
+        System.out.println(firstMusicPlayerSingleton == secondMusicPlayerSingleton);
+        System.out.println("Hash: " + firstMusicPlayerSingleton + "  " + secondMusicPlayerSingleton);
+        System.out.println("Volume: " + firstMusicPlayerSingleton.getVolume() + "  " + secondMusicPlayerSingleton.getVolume());
+        System.out.println("change volume");
+        firstMusicPlayerSingleton.setVolume(100);
+        System.out.println("Volume: " + firstMusicPlayerSingleton.getVolume() + "  " + secondMusicPlayerSingleton.getVolume());
+        System.out.println();
+
+        MusicPlayer firstMusicPlayerPrototype = context.getBean("musicPlayerPrototype", MusicPlayer.class);
+        MusicPlayer secondMusicPlayerPrototype = context.getBean("musicPlayerPrototype", MusicPlayer.class); //
+        System.out.println(firstMusicPlayerPrototype == secondMusicPlayerPrototype);
+        System.out.println("Hash: " + firstMusicPlayerPrototype + "  " + secondMusicPlayerPrototype);
+        System.out.println("Volume: " + firstMusicPlayerPrototype.getVolume() + "  " + secondMusicPlayerPrototype.getVolume());
+        System.out.println("change volume");
+        firstMusicPlayerPrototype.setVolume(100);
+        System.out.println("Volume: " + firstMusicPlayerPrototype.getVolume() + "  " + secondMusicPlayerPrototype.getVolume());
         context.close();
     }
 }
